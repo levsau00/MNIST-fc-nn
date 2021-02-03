aa0cc0313854b702ecae7cd35df15d5678d929cc
Lev Saunders <levsau00@gmail.com>
2021-02-03 16:03:04 -0500

    create forward function for froward prop

--- nn.py
+++ nn.py
@@ -2,8 +2,12 @@ import numpy as np
 
 class fcnn:
     def __init__(self):
-        self.layer1 = np.ndarray((1,784))
-        self.layer2 = np.ndarray((1,784))
-        self.layer3 = np.ndarray((1,10))
-        self.weight1= np.ndarray((1,784))
-        self.weight2= np.ndarray((1,784))
+        self.layer1 = np.ndarray((784,1))
+        self.layer2 = np.ndarray((10,1))
+        self.layer3 = np.ndarray((10,1))
+        self.w1 = np.ndarray((10,784))
+        self.w2 = np.ndarray((10,10))
+        self.b1 = np.ndarray((10,1))
+        self.b2 = np.ndarray((10,1))
+
+        
