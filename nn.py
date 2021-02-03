a6853c1ce0b0e3de6b4da9261ed4079f5ebf68aa
Lev Saunders <levsau00@gmail.com>
2021-02-03 09:32:44 -0500

    create fcnn class and init function

--- nn.py
+++ nn.py
@@ -1,2 +1,9 @@
 import numpy as np
 
+class fcnn:
+    def __init__(self):
+        self.layer1 = np.ndarray((1,784))
+        self.layer2 = np.ndarray((1,784))
+        self.layer3 = np.ndarray((1,10))
+        self.weight1= np.ndarray((1,784))
+        self.weight2= np.ndarray((1,784))
