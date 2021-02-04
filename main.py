243002ba2c89e785a23882a35c69fc0d3f3cb736
Lev Saunders <levsau00@gmail.com>
2021-02-04 11:08:02 -0500

    create to_onehot for labels

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import numpy as np
 import mnist
 import matplotlib.pyplot as plt
 
+
+def to_onehot(labels):
+    onehot = np.zeros((labels.size,10))
+    onehot[np.arange(labels.size),labels] = 1
+    return onehot
+
 def load_data():
     X_train = np.reshape(mnist.train_images(),(60000,784))
-    Y_train = mnist.train_labels()
+    Y_train = to_onehot(mnist.train_labels())
     X_test = np.reshape(mnist.test_images(),(10000,784))
-    Y_test = mnist.test_labels()
+    Y_test = to_onehot(mnist.test_labels())
     
     
     return X_train,Y_train,X_test,Y_test
@@ -14,7 +20,7 @@ def load_data():
 X_train,Y_train,X_test,Y_test = load_data()
 print(X_test.shape,X_train.shape)
 print(Y_test.shape,Y_train.shape)
-print(Y_test)
+
 
 
 
