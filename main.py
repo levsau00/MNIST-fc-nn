24e612883ea6ec40147195a31878ec87ac33148a
Lev Saunders <levsau00@gmail.com>
2021-02-09 14:08:32 -0500

    squash one-hot forward output bug

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def load_data():
     return X_train,Y_train,X_test,Y_test
 
 X_train,Y_train,X_test,Y_test = load_data()
-print(X_test.shape,X_train.shape)
-print(Y_test.shape,Y_train.shape)
+#print(X_test.shape,X_train.shape)
+#print(Y_test.shape,Y_train.shape)
 
 net = fcnn()
-print(net.forward(X_train[10]))
+print("forward output:",net.forward(X_train[10]))
 
 
--- nn.py
+++ nn.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 def relu(z): 
-    return np.maximum(z,0)
-
-def softmax(z): 
-    z-=np.max(z)
-    return np.exp(z)/np.sum(np.exp(z)) 
+    z = np.maximum(z,0)
+    return z
+def softmax(z):    
+    num = np.exp(z)
+    denom = np.sum(num)
+    return num/denom 
 
 def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
@@ -14,10 +15,10 @@ class fcnn:
     def __init__(self):
         self.layer1 = np.ndarray((10,1))
         self.layer2 = np.ndarray((10,1))
-        self.w1 = np.random.rand(10,784)
-        self.w2 = np.random.rand(10,10)
-        self.b1 = np.random.rand(10)
-        self.b2 = np.random.rand(10)
+        self.w1 = np.random.rand(10,784) - 0.5
+        self.w2 = np.random.rand(10,10) - 0.5
+        self.b1 = np.random.rand(10) - 0.5
+        self.b2 = np.random.rand(10) - 0.5
 
     
 
@@ -27,6 +28,6 @@ class fcnn:
 
         z2 = self.w2.dot(self.layer1) + self.b2
         self.layer2 = softmax(z2)
-        print(z1,z2)
+        print("z1:",z1,"\nz2:",z2)
         
         return self.layer2
